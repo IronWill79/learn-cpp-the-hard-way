136d7e2579360d94866da71513aba21bb87226d6
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-12-25 16:45:29 +1000

    Finish exercise 5

--- ex05.cpp
+++ ex05.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+    cout << "This is printed.\n";
+    cerr << "This writes to the error output.\n";
+
+    // notice the \t at the start
+    cout << "\tThis is tabbed in.\n";
+
     return 0;
 }
